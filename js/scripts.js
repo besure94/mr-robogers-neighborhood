9ac804eaafc2477bd8b053fd41dcf9ebfe6adf34
Brian Scherner <brianscherner@gmail.com>
2023-06-09 15:54:01 -0700

    Update mrRobogersNeighborhood function by removing lines 6-12 from 'if' conditional statement. Refactor code by converting index from 'for' loop to string and check to see if string includes a 3, 2, or 1. Function will add corresponding strings depending on which condition is met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,9 @@
-let userNumber = "13";
+let userNumber = "20";
 
 function mrRobogersNeighborhood(userNumber) {
 	let userOutputArray = [];
   for (let index = 0; index <= userNumber; index ++) {
-		if (index === 3) {
-			userOutputArray.push("Won't you be my neighbor?");
-		} else if (index === 2) {
-			userOutputArray.push("Boop!");
-		} else if (index === 1) {
-			userOutputArray.push("Beep!");
-		} else if (index.toString().includes('3')) {
+		if (index.toString().includes('3')) {
 			userOutputArray.push("Won't you be my neighbor?");
 		} else if (index.toString().includes('2')) {
 			userOutputArray.push("Boop!");
