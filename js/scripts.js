76c40e7f146a0a590de5e25c871ca33ff1960b48
Brian Scherner <brianscherner@gmail.com>
2023-06-09 13:55:22 -0700

    Add 'if' conditional statement that will add 'Won't you be my neighbor?' string to replace the number 3.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@ let userNumber = "5";
 function mrRobogersNeighborhood(userNumber) {
 	let userOutputArray = [];
   for (let index = 0; index <= userNumber; index ++) {
-    userOutputArray.push(index);
+		if (index === 3) {
+			userOutputArray.push("Won't you be my neighbor?");
+		} else {
+			userOutputArray.push(index);
+		}
   }
   return userOutputArray;
 }
